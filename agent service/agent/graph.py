a0237202524852d580c191b58fb2521d71263dd4
ddesilva <dashankadesilva@gmail.com>
2025-05-26 10:26:12 +0200

    Add Agent service

---
+++ agent service/agent/graph.py
@@ -0,0 +1,47 @@
+from langgraph.graph import StateGraph, START, END
+from langgraph.prebuilt import ToolNode
+from langgraph.checkpoint.memory import MemorySaver
+
+from agent.state import AgentState
+from agent.agent import agent
+from agent.tools import agent_tools
+
+
+# Condition function
+def should_continue(state: AgentState) -> str:
+    """Determine if we should use tools or end the conversation."""
+    messages = state["messages"]
+    last_message = messages[-1]
+    
+    # Check if the last message has tool calls
+    if hasattr(last_message, "tool_calls") and last_message.tool_calls:
+        return "tools"
+    return "end"
+
+
+def build_agent_graph():
+    """Build the ReAct agent graph."""
+    # Define Graph
+    workflow_graph = StateGraph(AgentState)
+
+    # Add nodes
+    workflow_graph.add_node("agent", agent)
+    workflow_graph.add_node("tools", ToolNode(agent_tools))
+
+    # Add edges
+    workflow_graph.add_edge(START, "agent")
+    workflow_graph.add_conditional_edges("agent",
+                                        should_continue,
+                                        {
+                                            "tools": "tools",
+                                            "end": END
+                                        }
+                                    )
+    workflow_graph.add_edge("tools", "agent")
+
+    # compile the graph
+    memory = MemorySaver() # Session memory
+    react_agent = workflow_graph.compile(checkpointer=memory)
+
+    return react_agent
+
---
+++ agent service/agent_service_app.py
@@ -0,0 +1,41 @@
+from fastapi import FastAPI
+#from api.routes import router
+from fastapi.middleware.cors import CORSMiddleware
+from fastapi import APIRouter, Request
+from pydantic import BaseModel
+from agent.graph import build_agent_graph
+from langchain_core.messages import HumanMessage
+
+
+app = FastAPI(title="Agent Service")
+
+# Allow CORS for all origins
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Allow all origins
+    allow_credentials=True,
+    allow_methods=["*"],  # Allow all methods
+    allow_headers=["*"],  # Allow all headers
+)
+
+
+# define agent
+react_agent = build_agent_graph()
+
+# Define input model for chat
+class ChatInput(BaseModel):
+    """Input data validation."""
+    user_message: str  # human message/query
+    session_id: str  # session id
+
+
+# Define the chat endpoint
+@app.post("/agent_respond")
+async def chat(input: ChatInput):
+    response = react_agent.invoke(
+        {"messages": [HumanMessage(content=input.user_message)]},
+        config={"configurable": {"thread_id": input.session_id}}
+    )
+    final_response = response['messages'][-1].content
+
+    return {"response": final_response}
